30ab95b7734f8927f323ac82508dfd44f72cd5e3
Boyce Shannon <B00688508@scm.net>
2018-03-05 12:49:12 +0000

    Initial commit

---
+++ app/src/main/java/com/example/b00688508/loginproject/ProfileActivity.java
@@ -0,0 +1,52 @@
+package com.example.b00688508.loginproject;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import java.io.IOException;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    TextView textView;
+    ImageView imageView;
+    EditText editText;
+
+    Uri uriProfileImage;
+    ProgressBar progressBar;
+
+    String profileImageUrl;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+}
